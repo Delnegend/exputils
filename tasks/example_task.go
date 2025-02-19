a86b1be03bdee3774f4590d219d14590ceb8513f
Delnegend <kiennguyen19323@gmail.com>
2025-02-20 01:52:45 +0700

    feat: more sophisticated example task

--- tasks/example_task.go
+++ tasks/example_task.go
@@ -2,14 +2,31 @@ package tasks
 
 import (
 	"context"
+	"fmt"
 	"time"
 )
 
-func ExampleTask(ctx context.Context) error {
-	select {
-	case <-ctx.Done():
-		return ctx.Err()
-	case <-time.After(5 * time.Second):
-		return nil
+func ExampleTask(ctx context.Context, setProgressChan chan float64, warnChan chan<- error) {
+	for i := 1; i < 4; i++ {
+		select {
+		case <-ctx.Done():
+			go func(cause error) {
+				warnChan <- cause
+			}(ctx.Err())
+			return
+		case setProgressChan <- float64(i) / 3:
+		}
+
+		select {
+		case <-ctx.Done():
+			go func(cause error) {
+				warnChan <- cause
+			}(ctx.Err())
+			return
+		case <-time.After(time.Second):
+			go func(i int) {
+				warnChan <- fmt.Errorf("example warning %d", i)
+			}(i)
+		}
 	}
 }
